45da7e551c0eacc98c8c2641fe2537a76b0171a9
leo soto <leonelsoto17@gmail.com>
2024-01-15 18:07:47 -0600

    incorporated deletion and updating people into the backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const Person = require('./models/person')
+const People = require('./models/person')
 
 morgan.token('type', function(req, res) { 
     return  JSON.stringify(req.body)
@@ -12,7 +12,15 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :t
 app.use(express.static('build'))
 
 
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
 
+    if( error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    }
+
+    next(error)
+}
 
 
 let phonebook = [
@@ -37,25 +45,34 @@ let phonebook = [
       "number": "39-23-6423122"
     }
 ]
-let infoResponse = `<p>Phonebook has info for ${phonebook.length} people </p> <br /> ${Date()}`
+let infoResponse = `<p>Phonebook has info for ${People.length} people </p> <br /> ${Date()}`
 let generateId = () => Math.floor(Math.random() * 99999999999999)
 
 app.get('/api/people', (request, response) => {
-    Person.find({}).then(people => {
+    
+    People.find({}).then(people => {
         response.json(people)
     })
 })
 app.get('/info', (request, response) => {
     response.send(infoResponse)
 })
-app.get('/api/people/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.get('/api/people/:id', (request, response, next) => {
+
+
+    People.findById(request.params.id)
+        .then(person => {
+            if(person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/people/:id', (request, response) => {
-    Person.findByIdAndDelete(request.params.id)
+    People.findByIdAndDelete(request.params.id)
         .then(result => {
             response.status(204).end()
         })
@@ -68,7 +85,7 @@ app.post('/api/people', (request, response) => {
         return response.status(400).json({ error: 'content missing' })
     }
 
-    const person = new Person({
+    const person = new People({
         name: body.name,
         number: body.number,
     })
@@ -78,6 +95,23 @@ app.post('/api/people', (request, response) => {
     })
 })
 
+app.put('/api/people/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+console.log('test');
+    People.findByIdAndUpdate(request.params.id, person, { new: true} )
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('server run');
